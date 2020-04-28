3ea7a6ca9d8c073599131fa30f0f1787bc65d5ab
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-28 15:17:21 -0400

    in PhotoList.js, imported React, useState, useEffect, and axios; made PhotoList function, set up state for the data; default exported PhotoList function

---
+++ src/components/PhotoList.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function PhotoList() {
+    const [photos, setPhotos] = useState([]);
+}
+
+export default PhotoList;
