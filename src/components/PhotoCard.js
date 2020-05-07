e30b7740827d43ad0632bf62379257c4a4899825
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-06 21:57:52 -0400

    redid the h2 and the text container with styled components

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./styles.css"
 
+import styled from 'styled-components';
+
+const BlackH2 = styled.h2`
+    padding: 40px 0 20px 0;
+`
+
+const SpacedText = styled.div`
+    text-align: left;
+    width: 90%;
+    margin: auto;
+    padding: 20px 0 40px 0;
+`
+
 function PhotoCard(props) {
     return (
         <div className="photoCard">
-            <h2>{props.title}</h2>
+            <BlackH2>{props.title}</BlackH2>
             <img src={props.url}></img>
-            <div className="textContent">
+            <SpacedText className="textContent">
                 <p>Date: {props.date}</p>
                 <p>{props.explanation}</p>
-            </div>
+            </SpacedText>
         </div>
     );
 };
