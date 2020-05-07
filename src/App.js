5182ea098abd883d0db3b5c7c424405576cd3249
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-06 21:49:14 -0400

    installed styled-components, styled the h1 with styled-components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import PhotoList from "./components/PhotoList.js";
 import "./components/styles.css";
 
+import styled from 'styled-components';
+
+const WhiteH1 = styled.h1`
+  color: white;
+  height: 10vh;
+  margin: 45vh 0;
+`
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +18,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p> */}
-      <h1>NASA Astronomy Photo of the Day</h1>
+      <WhiteH1>NASA Astronomy Photo of the Day</WhiteH1>
       <PhotoList />
       <p className="bgImgCredit">Background Photo by Ivana Cajina on Unsplash</p>
     </div>
